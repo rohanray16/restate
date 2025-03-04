cd8114bef9b7acec9ce4f122c391e557b56ca93a
rohanray16 <rohanandmail@gmail.com>
2025-03-04 22:39:10 +0530

    first commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-3xl font-rubik my-10">Welcome to ReState</Text>
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/[id]">Property</Link>
+    </View>
+  );
+}
